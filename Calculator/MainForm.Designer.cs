896f1c56a15b5b8a60d578537e5d6fbb594e8446
Ziauddin <ziauddin1234@gmail.com>
2020-08-13 04:28:47 +0500

    initial

---
+++ Calculator/MainForm.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Calculator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbExpression = new System.Windows.Forms.TextBox();
+            this.lstLog = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuLog = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.removeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuLog.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbExpression
+            // 
+            this.tbExpression.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tbExpression.Location = new System.Drawing.Point(0, 269);
+            this.tbExpression.Name = "tbExpression";
+            this.tbExpression.Size = new System.Drawing.Size(263, 20);
+            this.tbExpression.TabIndex = 0;
+            this.tbExpression.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbExpression_KeyDown);
+            // 
+            // lstLog
+            // 
+            this.lstLog.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.lstLog.ContextMenuStrip = this.contextMenuLog;
+            this.lstLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstLog.FullRowSelect = true;
+            this.lstLog.GridLines = true;
+            this.lstLog.Location = new System.Drawing.Point(0, 0);
+            this.lstLog.Name = "lstLog";
+            this.lstLog.Size = new System.Drawing.Size(263, 269);
+            this.lstLog.TabIndex = 1;
+            this.lstLog.UseCompatibleStateImageBehavior = false;
+            this.lstLog.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Expression / Info";
+            this.columnHeader1.Width = 174;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Result";
+            this.columnHeader2.Width = 85;
+            // 
+            // contextMenuLog
+            // 
+            this.contextMenuLog.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.removeToolStripMenuItem,
+            this.clearToolStripMenuItem});
+            this.contextMenuLog.Name = "contextMenuLog";
+            this.contextMenuLog.Size = new System.Drawing.Size(118, 48);
+            this.contextMenuLog.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.contextMenuLog_ItemClicked);
+            // 
+            // removeToolStripMenuItem
+            // 
+            this.removeToolStripMenuItem.Name = "removeToolStripMenuItem";
+            this.removeToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.removeToolStripMenuItem.Text = "Remove";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 289);
+            this.Controls.Add(this.lstLog);
+            this.Controls.Add(this.tbExpression);
+            this.Name = "MainForm";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.contextMenuLog.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbExpression;
+        private System.Windows.Forms.ListView lstLog;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuLog;
+        private System.Windows.Forms.ToolStripMenuItem removeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+    }
+}
+
---
+++ Calculator/MainForm.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class MainForm : Form
+    {
+        public void Evaluate(string expression)
+        {
+            if(expression != null && expression.Length >= 0)
+            {
+                NCalc.Expression exp = new NCalc.Expression(expression);
+                object eval = null;
+                try
+                {
+                    eval = exp.Evaluate();
+                }
+                catch(ArgumentException)
+                {
+                    this.AddList("Error in the expression.");
+                    return;
+                }
+                catch(EvaluateException e)
+                {
+                    this.AddList("Error while evaluating expression, Error Message" + e.Message);
+                    return;
+                }
+                
+                if(exp.HasErrors())
+                {
+                    this.AddList("Error while evaluating expression, Error Message" + exp.Error);
+                }
+                else
+                {
+                    this.AddList(expression, eval);
+                }
+            }
+        }
+        public void AddList(string expression, object result)
+        {
+            this.lstLog.Items.Add(this.GetLogListItem(expression, result.ToString(), null, Color.Green));
+        }
+        public void AddList(string error)
+        {
+            this.lstLog.Items.Add(this.GetLogListItem(error, "<ERROR>", null, Color.Red));
+        }
+        public ListViewItem GetLogListItem(string text1, string text2, Color? b_color = null, Color? f_color = null)
+        {
+            ListViewItem item = new ListViewItem();
+            item.Text = text1;
+            item.SubItems.Add(text2);
+
+            if (b_color != null && b_color.HasValue)
+                item.BackColor = b_color.Value;
+
+            if (f_color != null && f_color.HasValue)
+                item.ForeColor = f_color.Value;
+
+            return item;
+        }
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void tbExpression_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                string expression = this.tbExpression.Text;
+                this.tbExpression.Text = "";
+                this.Evaluate(expression);
+            }
+        }
+
+        private void contextMenuLog_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            this.contextMenuLog.Close();
+            if(e.ClickedItem == this.removeToolStripMenuItem)
+            {
+                var selected_items = this.lstLog.SelectedItems;
+                if(selected_items != null && selected_items.Count > 0)
+                {
+                    var selected_item = selected_items[0];
+                    this.lstLog.Items.Remove(selected_item);
+                }
+            }
+            else if(e.ClickedItem == this.clearToolStripMenuItem)
+            {
+                this.lstLog.Items.Clear();
+            }
+        }
+    }
+}
